6a2fa41ba2704f0c8e9b0ac947a9a520af9c4f8d
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-09 19:18:17 +0800

    Day 001

---
+++ Irfan.java
@@ -0,0 +1,23 @@
+package com.mycompany.irfan;
+
+public class Irfan {
+
+    public static void main(String[] args) {
+    
+        
+        int angka=1;
+        double desimal=1.0;
+        char huruf = 'A';
+        boolean benar = true;
+        boolean salah = false;
+        String nama="Irfan Jayadi";
+        
+        
+        
+        System.out.println(angka);
+        System.out.println(desimal);
+        System.out.println(huruf);
+        System.out.println(benar);
+        System.out.println(nama);
+    }
+}
