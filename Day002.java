7516d106d1a9c744d581decaef140c22f766c413
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-10 17:22:46 +0800

    Day 002

---
+++ Day002.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 5;
+		
+		int c = (a * b);
+		System.out.println ("hasil dari "+a+"*"+b+ " adalah "+c);
+	}
+}
