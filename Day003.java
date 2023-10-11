e4ea88a30df895da4798614513d21c9de7d009ca
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-11 19:04:08 +0800

    Day 003

---
+++ Day003.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		double a = 3.5;
+		double b = 2.6;
+		double c = 4.1;
+		
+		double d =a+b*c;
+		
+		System.out.print("Hasil dari "+a+" + "+b+" * "+c+" = "+d);
+		
+	}
+}
