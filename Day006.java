86679cde48202db74cca5f66ace04b74eb3d0460
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-14 16:30:20 +0800

    Day 006

---
+++ Day006.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Jika ");
+     int a= input.nextInt();
+        System.out.print("ditambah ");
+     int b= input.nextInt();
+     
+      System.out.print("Maka hasilnya = ");
+      System.out.print(a+b);
+	}
+}
