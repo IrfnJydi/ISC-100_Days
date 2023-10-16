1aa9b887b804ae884c027308d9951b81b569e195
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-16 23:23:01 +0800

    Day 008

---
+++ Day008.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double nilai = sc.nextDouble();
+        if(nilai>50){
+            System.out.println("LULUS");
+        }else{
+            System.out.println("TIDAK LULUS");
+    }
+    
+}
+    }
