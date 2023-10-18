d05cf583f06281e7a70d67001c3d2d63065af8d9
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-18 22:23:18 +0800

    Day 010

---
+++ Day010.java
@@ -0,0 +1,14 @@
+package day010;
+
+public class Day010 {
+
+    public static void main(String[] args) {
+        System.out.println("Irfan Jayadi");
+        System.out.println(12.3456);
+        System.out.println();
+        System.out.println(123.45f);
+        System.out.println(12);
+        System.out.println(1*1+1-1);
+    }
+    
+}
