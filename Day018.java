7293064167a44ff665f10881c47bcd0a9ea2c4b7
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-10-26 23:34:12 +0800

    Day 018

---
+++ Day018.java
@@ -0,0 +1,14 @@
+public class PretestPraktikum4 {
+    public static void main(String[] args) {
+        //Nomor8
+
+        int x = 22;
+        int y = 7;
+
+        int z = x;
+        x=y;
+        y=z;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
