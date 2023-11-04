4e187707da39c674acbfc1c26e6a62907c1cff1c
Irfan Jayadi <147225421+IrfnJydi@users.noreply.github.com>
2023-11-04 22:49:53 +0800

    Day 027

---
+++ Day27.java
@@ -0,0 +1,11 @@
+
+public class Day27 {
+
+    public static void main(String[] args) {
+        String[] kunci = {"Java", "Sumatera", "Kalimantan", "Sulawesi"};
+        String kunciKemenangan = kunci[0];
+
+        System.out.println("Kunci kemenangan adalah " + kunciKemenangan + " barang siapa ingin menang dia harus menguasai " + kunciKemenangan);
+    }
+
+}
